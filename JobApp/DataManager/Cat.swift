aff11196dd7e78d7423e82a9e4156f7bd37d054b
Reza Shirazian <reza.shirazian@gmail.com>
2017-05-21 18:07:42 -0700

    Cat model setup
    We setup the cat model to holds the values it gets from the remote server. If no values are returned defaults are set

--- JobApp/DataManager/Cat.swift
+++ JobApp/DataManager/Cat.swift
@@ -18,9 +18,19 @@ public protocol Model {
 
 public struct Cat: Model {
 
-    public init?(data: Data) {
-        guard !data.isEmpty else {
-            return nil
-        }
-    }
+  public let name: String
+  public let source: String
+  public let image: String
+  public let caption: String
+  
+  public init?(data: Data) {
+      guard !data.isEmpty else {
+          return nil
+      }
+    
+    self.name = data["name"] as? String ?? "Unnamed Kitty"
+    self.source = data["source"] as? String ?? "No Source"
+    self.image = data["image"] as? String ??  "https://maxcdn.icons8.com/Color/PNG/512/User_Interface/cat_profile-512.png"
+    self.caption = data["caption"] as? String ?? "No caption"
+  }
 }
